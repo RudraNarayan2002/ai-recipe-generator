450fd4148caa5db86f684ab47d58acc79f6763f7
RudraNarayan2002 <brnsubudhi1151@gmail.com>
2025-11-15 23:21:47 +0530

    file name changes

--- src/app/components/RecipeDIsplayPage.tsx
+++ src/app/components/RecipeDIsplayPage.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { experimental_useObject as useObject } from "@ai-sdk/react";
 import { recipeSchema } from "@/app/api/structured-data/schema";
 
-export default function RecipeDIsplayPage() {
+export default function RecipeDisplayPage() {
   const [dishName, setDishName] = useState("");
 
   const { submit, object, isLoading, error, stop } = useObject({
